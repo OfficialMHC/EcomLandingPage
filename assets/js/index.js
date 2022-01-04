fcb363f21e3beeeeb990835e50ca234a55496a97
OfficialMHC <official.dev.mhc@gmail.com>
2022-01-05 00:17:49 +0600

    Initial Commit

---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+
+
+// TOGGLE NAVBAR //
+const bar = document.getElementById('bar');
+const cartMobile = document.getElementById('cart-mobile');
+const nav = document.getElementById('navbar');
+const close = document.getElementById('close');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+        cartMobile.classList.add('none');
+    });
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+        cartMobile.classList.remove('none');
+    });
+}
